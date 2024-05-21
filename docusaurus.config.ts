5b8aecf6723e62432485c2f08bc4c9f6c2f4dd71
dk-invest-101 <example@example.com>
2024-05-21 08:55:44 +0200

    rm file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,50 +1,52 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'DK Invest 101',
-  tagline: 'Basics for investing in Denmark',
+  title: "DK Invest 101",
+  tagline: "Basics for investing in Denmark",
+
+  favicon: '/img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://dk-invest-101.github.io/',
+  url: "https://dk-invest-101.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dk-invest-101', // Usually your GitHub org/user name.
-  projectName: 'dk-invest-101.github.io', // Usually your repo name.
+  organizationName: "dk-invest-101", // Usually your GitHub org/user name.
+  projectName: "dk-invest-101.github.io", // Usually your repo name.
   // deploymentBranch: "deployment",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/', // Serve the docs at the site's root
-          sidebarPath: './sidebars.ts',
+          routeBasePath: "/", // Serve the docs at the site's root
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dk-invest-101/dk-invest-101.github.io/tree/main/',
+            "https://github.com/dk-invest-101/dk-invest-101.github.io/tree/main/",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -52,17 +54,20 @@ const config: Config = {
 
   themeConfig: {
     navbar: {
-      title: 'DK Invest 101',
+      title: "DK Invest 101",
+      logo: {
+        src: "img/invest-101.svg",
+      },
       items: [
         {
-          href: 'https://github.com/dk-invest-101/DK-Investing-101',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/dk-invest-101/DK-Investing-101",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'light',
+      style: "light",
       links: [
         // {
         //   title: 'Docs',
@@ -74,7 +79,7 @@ const config: Config = {
         //   ],
         // },
       ],
-      copyright: `MIT Licensed © ${new Date().getFullYear()}`,
+      copyright: `For legal reasons: This website is no financial advice :)<br/>MIT Licensed © ${new Date().getFullYear()}`,
     },
     prism: {
       theme: prismThemes.github,
